a11d6d996af006ae34077a20a865610565f65721
juliamaheoi <juliaramosguedes@gmail.com>
2023-12-23 14:11:32 -0300

    chore: update packages

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -56,8 +56,7 @@ export default async function handler(req: NextRequest) {
             style={{
               position: 'absolute',
               width: '100%',
-              height: '100%',
-              objectFit: 'cover'
+              height: '100%'
             }}
           />
         )}
